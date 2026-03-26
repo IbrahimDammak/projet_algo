b96df886dea406cecd73fe3b9acbec71993778fa
IbrahimDammak <brobarhomdamak@gmail.com>
2026-03-26 16:28:28 +0100

    add initial implementation of hashing functions in hachage.c

---
+++ hachage.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char* sha256file(char* file);
+char* hashToString(char* hash);
+void blobFile(char* file);
+
+
+
+
+char* sha256file(char* file){
+    return NULL;
+}
+
+
+char* hashToString(char* hash){
+
+}
+
+void blobFile(char* file){
+
+}
